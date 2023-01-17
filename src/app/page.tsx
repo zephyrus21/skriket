ba671e6e5065e9dc6b113648c715350fc216723f
zephyrus21 <pandey.piyus@gmail.com>
2023-01-17 12:40:37 +0530

    added features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { CirclePicker } from "@hello-pangea/color-picker";
+import { useDraw } from "@/hooks/useDraw";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({}) => {
+  const [color, setColor] = useState<string>("#000");
+  const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+
+  function drawLine({ ctx, currentPoint, prevPoint }: Draw) {
+    const { x: currentX, y: currentY } = currentPoint;
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currentX, currentY);
+    ctx.stroke();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
+    <div className='h-screen w-screen flex justify-center items-center gap-8'>
+      <div className='flex flex-col gap-4'>
+        <CirclePicker color={color} onChange={(e) => setColor(e.hex)} />
+        <button
+          onClick={clear}
+          className='p-4 bg-slate-500 text-white rounded-md'>
+          Clear
+        </button>
+      </div>
       <canvas
+        onMouseDown={onMouseDown}
+        ref={canvasRef}
         width={750}
         height={750}
-        className='border border-black rounded-lg'
+        className='border border-black rounded-md'
       />
     </div>
   );
--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -1,1 +1,63 @@
-export const useDraw = () => {};
+import { useEffect, useRef, useState } from "react";
+
+export const useDraw = (
+  onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void
+) => {
+  const [mouseDown, setMouseDown] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const prevPointRef = useRef<Point | null>(null);
+
+  const onMouseDown = () => {
+    setMouseDown(true);
+  };
+
+  const clear = () => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  };
+
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (!mouseDown) return;
+      const currentPoint = computePointInCanvas(e);
+
+      const ctx = canvasRef.current?.getContext("2d");
+      if (!ctx || !currentPoint) return;
+
+      onDraw({ ctx, currentPoint, prevPoint: prevPointRef.current });
+      prevPointRef.current = currentPoint;
+    };
+
+    const computePointInCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      return { x, y };
+    };
+
+    const mouseUpHandler = () => {
+      setMouseDown(false);
+      prevPointRef.current = null;
+    };
+
+    const canvas = canvasRef.current;
+
+    //# Add event listener to the canvas
+    canvas?.addEventListener("mousemove", handler);
+    window.addEventListener("mouseup", mouseUpHandler);
+
+    //# Remove event listener on cleanup
+    return () => {
+      canvas?.removeEventListener("mousemove", handler);
+      window.removeEventListener("mouseup", mouseUpHandler);
+    };
+  }, [onDraw]);
+
+  return { canvasRef, onMouseDown, clear };
+};
