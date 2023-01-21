fa4ee8ddfcad5be20e3383c49c0d2d02f23b889e
zephyrus21 <pandey.piyus@gmail.com>
2023-01-21 12:29:00 +0530

    a

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 import { Server } from "socket.io";
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://skriket.zephyrus.tech/",
   },
 });
 
