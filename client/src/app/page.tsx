066b1b4fed04db48a4c99344d5d7bb0627dc1c2a
zephyrus21 <pandey.piyus@gmail.com>
2023-01-21 11:18:05 +0530

    added funcs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,67 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CirclePicker } from "@hello-pangea/color-picker";
 import { useDraw } from "@/hooks/useDraw";
+import { io } from "socket.io-client";
+import { drawLine } from "@/utils/drawLine";
+
+const socket = io("http://localhost:3001");
 
 interface HomeProps {}
 
+type DrawLineProps = {
+  prevPoint: Point | null;
+  currentPoint: Point;
+  color: string;
+};
+
 const Home: React.FC<HomeProps> = ({}) => {
   const [color, setColor] = useState<string>("#000");
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
-  function drawLine({ ctx, currentPoint, prevPoint }: Draw) {}
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
 
+    socket.emit("client-ready");
+
+    socket.on("get-canvas-state", () => {
+      if (!canvasRef.current?.toDataURL()) return;
+      console.log("sending canvas state");
+      socket.emit("canvas-state", canvasRef.current.toDataURL());
+    });
+
+    socket.on("canvas-state-from-server", (state: string) => {
+      console.log("I received the state");
+      const img = new Image();
+      img.src = state;
+      img.onload = () => {
+        ctx?.drawImage(img, 0, 0);
+      };
+    });
+
+    socket.on(
+      "draw-line",
+      ({ prevPoint, currentPoint, color }: DrawLineProps) => {
+        if (!ctx) return console.log("no ctx here");
+        drawLine({ prevPoint, currentPoint, ctx, color });
+      }
+    );
+
+    socket.on("clear", clear);
+
+    return () => {
+      socket.off("draw-line");
+      socket.off("get-canvas-state");
+      socket.off("canvas-state-from-server");
+      socket.off("clear");
+    };
+  }, [canvasRef]);
+
+  function createLine({ prevPoint, currentPoint, ctx }: Draw) {
+    socket.emit("draw-line", { prevPoint, currentPoint, color });
+    drawLine({ prevPoint, currentPoint, ctx, color });
+  }
   return (
     <div className='h-screen w-screen flex justify-center items-center gap-8'>
       <div className='flex flex-col gap-4'>
